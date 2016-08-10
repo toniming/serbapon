d1a58c0ed60b70d1d3c43d224ed2ad17ae02ef45
toniming <311310023@student.machung.ac.id>
2016-08-10 10:10:38 +0700

    file baru

---
+++ resources/views/cms/pages/website/website.blade.php
@@ -0,0 +1,28 @@
+@extends('cms.template')
+@section('content')
+<ul class="nav nav-tabs" id="detail-menu " role="tablist">
+	<li class="nav-item active">    
+		 <a class="nav-link" data-toggle="tab" href="#home" role="tab">Subscribber</a>
+	</li>
+	<li class="nav-item">
+		<a class="nav-link" data-toggle="tab" href="#profile" role="tab">Newsletter</a>
+	</li>
+	<!-- cont. here	-->
+	</ul>   
+	<div class="row" style="margin-top: -2px;">
+		<div class="col-sm-12">
+			<div class="card card-block">
+				<div class="tab-content">
+					<div class="tab-pane fade in active" id="home" role="tabpanel">
+						1
+					</div>
+				  	<div class="tab-pane fade" id="profile" role="tabpanel">
+				  		2
+				  	</div>
+					<!-- cont. here	-->
+				</div>          
+			</div>          
+		</div>          
+	</div> 
+</div>
+@stop
